d2555da2e5f0287dd7e48e64ed1908f957bbe760
Sherry Martin <sm674@cornell.edu>
2018-10-21 15:58:44 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,9 +3,9 @@ require 'active_support/inflector'
 
 class InteractiveRecord
 
- #  def self.table_name
- #   self.to_s.downcase.pluralize
- # end
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
 
 
 end
